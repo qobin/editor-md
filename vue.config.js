b6c2330561893445c53ef4f1ec53d23c7860b387
qobin <onepigtk@163.com>
2023-02-14 23:45:14 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -3,24 +3,7 @@ module.exports = {
     // 选项...
     publicPath: './',
     outputDir: 'editor-md',
-    configureWebpack: {
-        plugins: [
-            // 配置jQuery
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery",
-                "windows.jQuery": "jquery",
-            }),
-            // 开启gzip压缩
-            new CompressionPlugin({
-                algorithm: 'gzip',
-                test: /\.(js|css)$/,// 匹配文件名
-                threshold: 10240, // 对超过10k的数据压缩
-                deleteOriginalAssets: false, // 不删除源文件
-                minRatio: 0.8 // 压缩比
-            })
-        ],
-    },
+
     chainWebpack: config => {
         config.module
             .rule('md')
