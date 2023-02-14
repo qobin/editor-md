831a3f91e9f1e38c931c42fbc0da7432dd97c8db
10002927 <qobin.quan@shein.com>
2023-02-14 20:16:04 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,32 @@
 // vue.config.js
 module.exports = {
-  // 选项...
-  publicPath: './',
-  outputDir: 'editor-md',
-  chainWebpack: config => {
-    config.module
-      .rule('md')
-      .test(/.md$/)
-      .use('markdown-loader')
-      .loader('markdown-loader')
-      .loader('html-loader')
-  }
+    // 选项...
+    publicPath: './',
+    outputDir: 'editor-md',
+    configureWebpack: {
+        plugins: [
+            // 配置jQuery
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                "windows.jQuery": "jquery",
+            }),
+            // 开启gzip压缩
+            new CompressionPlugin({
+                algorithm: 'gzip',
+                test: /\.(js|css)$/,// 匹配文件名
+                threshold: 10240, // 对超过10k的数据压缩
+                deleteOriginalAssets: false, // 不删除源文件
+                minRatio: 0.8 // 压缩比
+            })
+        ],
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('md')
+            .test(/.md$/)
+            .use('markdown-loader')
+            .loader('markdown-loader')
+            .loader('html-loader')
+    }
 }
